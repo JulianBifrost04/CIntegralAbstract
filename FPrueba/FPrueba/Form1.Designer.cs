cabfe11a9821c75c2f994cc6058cc6a6de08eda0
JulianBifrost04 <jjaq.1994@gmail.com>
2016-05-21 18:43:25 -0600

    Probando2

--- FPrueba/FPrueba/Form1.Designer.cs
+++ FPrueba/FPrueba/Form1.Designer.cs
@@ -34,8 +34,9 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(169, 49);
+            this.button1.Location = new System.Drawing.Point(131, 83);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(201, 96);
             this.button1.TabIndex = 0;
