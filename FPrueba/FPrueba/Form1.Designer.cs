d0e37281fb472cb0cbe7b09105218eeedf25b7ee
JulianBifrost04 <jjaq.1994@gmail.com>
2016-05-21 18:26:44 -0600

    Nuevo

--- FPrueba/FPrueba/Form1.Designer.cs
+++ FPrueba/FPrueba/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(201, 96);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Mi Primer Respositorio";
+            this.button1.Text = "Mi Segundo Repositorio";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
