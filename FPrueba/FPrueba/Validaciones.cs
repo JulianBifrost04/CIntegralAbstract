fb9c5afb46c7ca87e35af84ce4079b404451274a
JulianBifrost04 <jjaq.1994@gmail.com>
2016-05-21 19:07:33 -0600

    Validaciones

---
+++ FPrueba/FPrueba/Validaciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPrueba
+{
+    class Validaciones
+    {
+        // Estamos Probando la inserccion de Clases
+    }
+}
